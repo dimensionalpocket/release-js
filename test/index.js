6d0cd9c814c8bee47006a9aa1ae85be3402f9cfe
Henry Brisson <63800702+henry-brisson@users.noreply.github.com>
2021-08-27 23:15:37 -0400

    feat: add support for both default and defaults (#4)
    Co-authored-by: pauloddr <pauloddr@users.noreply.github.com>

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
 const { expect } = require('@dimensionalpocket/development')
 
 const defaults = require('../')
-const defaultsFromConfig = require('../config/defaults')
+const defaultsFromConfig = require('../config/default')
 
 describe('main require', function () {
   it('exports defaults from config', function () {
